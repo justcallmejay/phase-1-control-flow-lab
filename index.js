a71abcc2484a9bb4ada6072dbe5fd34e682392d7
jay <jaypifiverr@gmail.com>
2022-06-27 21:46:45 -0400

    commit tree 2b2ae49dafb20d8bbf5945003fb6df0d219908ac

--- index.js
+++ index.js
@@ -1,11 +1,32 @@
-function scuberGreetingForFeet(){
-  // Write your code here!
+function scuberGreetingForFeet(insertNumber){
+  if (insertNumber <= 400){
+    return 'This one is on me!'  
+  } else if (insertNumber >= 400 && insertNumber < 2000){
+    return 'That will be twenty bucks.'
+  } else if (insertNumber >= 2000 && insertNumber < 2499) {
+    return 'I will gladly take your thirty bucks.'
+  } else (insertNumber > 2500)
+    return 'No can do.'
 }
 
-function ternaryCheckCity(){
-  // Write your code here!
-}
+console.log(scuberGreetingForFeet(2501));
 
-function switchOnCharmFromTip(){
-  // Write your code here!
+function ternaryCheckCity(location){
+  if (location === 'NYC'){
+    return 'Ok, sounds good.'
+  } else if (location === 'Pittsburgh'){
+    return 'No go.'
+  }
 }
+console.log(ternaryCheckCity(NYC))
+
+function switchOnCharmFromTip(tipType){
+  if (tipType === 'generous'){
+    return 'Thank you so much.'
+  } else if (tipType === 'not as generous'){
+    return 'Thank you.'
+  } else if (tipType === 'thanks for everything') {
+    return 'Bye.'
+  }
+}
+console.log(switchOnCharmFromTip('Bye'))
